b6f4423f34783f304894069c8225919336c884a9
TusharSupanekar <tsupanekar2810@gmail.com>
2023-08-10 10:00:15 +0530

    Java OOPS programs

--- objectclass/Car.java
+++ objectclass/Car.java
@@ -1,4 +1,5 @@
 package com.jsp.objectclass;
+
 import java.util.Objects;
 
 public class Car {
@@ -6,23 +7,25 @@ public class Car {
 	String brand;
 	String name;
 	double price;
+	String color;
 	
-	public Car(String brand,String name,double price) {
+	public Car(String brand,String name,double price,String color) {
 		this.brand=brand;
 		this.name=name;
 		this.price=price;
+		this.color=color;
 	}
 	
 	@Override
 	public String toString() {
-		return brand+" is car brand " +  name+" is car name "+price+" is car price";
+		return brand+" is car brand " +  name+" is car name "+price+" is car price "+color+" is car color";
 	}
 	
 	@Override
 	public boolean equals(Object obj) {
 		Car c=(Car)obj;
 		
-		if(this.brand==c.brand && this.name==c.name && this.price==c.price) {
+		if(this.brand==c.brand && this.name==c.name && this.price==c.price && this.color==c.color) {
 			return true;
 		}else {
 			return false;
@@ -31,7 +34,7 @@ public class Car {
 	}
 	@Override
 	public int hashCode(){
-		return Objects.hash(brand,name,price);
+		return Objects.hash(brand,name,price,color);
 	}
 
 }
