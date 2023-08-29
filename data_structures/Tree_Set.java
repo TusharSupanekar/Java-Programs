afa2143e058062123e775be43427260070507302
TusharSupanekar <tsupanekar2810@gmail.com>
2023-08-29 09:58:37 +0530

    Tree_set

---
+++ data_structures/Tree_Set.java
@@ -0,0 +1,18 @@
+package com.jsp.data_structures;
+
+import java.util.TreeSet; 
+public class Tree_Set {
+	public static void main(String[] args) {
+		TreeSet tr=new TreeSet();
+		tr.add(560);
+		tr.add(2);
+		tr.add(45);
+		tr.add(20);
+		tr.add(850);
+		System.out.println(tr);
+	}
+	
+	
+	
+}
+
